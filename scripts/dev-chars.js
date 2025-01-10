d923c41c49282b4815fc859711fcba0e15879e10
E. Dunham <github@edunham.net>
2025-01-10 11:31:00 -0800

    fix PowerShell like we fix GitHub

--- scripts/dev-chars.js
+++ scripts/dev-chars.js
@@ -27,6 +27,7 @@ fs.readdir( postsDir, (err, files) => {
     var count_urls = (contents.match(/\(https:\/\/developer\.okta\.com\/blog\/(.+)\)/g) || []).length;
 
     var count_github_case =  (contents.match(/Github/g) || []).length;
+    var count_powershell_case =  (contents.match(/[Pp]owershell/g) || []).length;
 
     console.log("Reading latest post "+fn);
     console.log("Found "+count_quotes+" curly quotes, "+count_urls+" absolute blog URLs, and "+count_github_case+" 'GitHub' case issues.");
@@ -44,11 +45,14 @@ fs.readdir( postsDir, (err, files) => {
             console.log("");
             console.log("Replaced "+count_quotes+" characters");
 
-            contents = contents.replace(/\(https:\/\/developer\.okta\.com\/blog\/(.+)\)/g, '(/blog/$1)');
+            contents = contents.replace(/\(http[s]:\/\/developer\.okta\.com\/blog\/(.+)\)/g, '(/blog/$1)');
             console.log("Replaced "+count_urls+" instances of absolute blog URLs");
 
             contents = contents.replace(/Github/g, 'GitHub');
             console.log("Replaced "+count_github_case+" instances of 'Github' to 'GitHub'");
+
+            contents = contents.replace(/[Pp]owershell/g, 'PowerShell');
+            console.log("Fixed "+count_powershell_case+" PowerShell capitalization errors");
 
             fs.writeFileSync(postsDir+'/'+fn, contents);
 
